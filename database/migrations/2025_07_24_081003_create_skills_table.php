37ef813d4e35226d81fa380f98b5ecff038ce968
momen162 <miah15-4789@diu.edu.bd>
2025-07-24 14:23:08 +0600

    database connecting

--- database/migrations/2025_07_24_081003_create_skills_table.php
+++ database/migrations/2025_07_24_081003_create_skills_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('skills', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone');
-            $table->string('avatar')->nullable();
-            $table->string('student_id')->unique();
+            $table->enum('type', ['technical', 'soft']);
+            $table->enum('level', ['beginner', 'intermediate', 'expert']);
+            $table->string('logo')->nullable();
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('skills');
     }
 };
--- database/migrations/2025_07_24_081004_create_infos_table.php
+++ database/migrations/2025_07_24_081004_create_infos_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('infos', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone');
-            $table->string('avatar')->nullable();
-            $table->string('student_id')->unique();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('portfolio')->nullable();
+            $table->string('address')->nullable();
+            $table->text('description')->nullable();
+            $table->string('designation')->nullable();
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('infos');
     }
 };
